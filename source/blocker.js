71fdf66abf0c784489fd7f5142dce7120d5357e5
Mark McCanna <markmccanna1@gmail.com>
2014-11-10 20:06:12 -0600

    styled spinner

--- source/blocker.js
+++ source/blocker.js
@@ -2,6 +2,6 @@ enyo.kind({
   name: 'bootstrap.Blocker',
   classes: 'onyx-scrim enyo-fit onyx-scrim-translucent spinner-container',
   components: [
-    { classes: 'fa fa-spinner fa-spin spinner', style: 'color: #636363' }
+    { classes: 'fa fa-spinner fa-spin spinner' }
   ]
 });
