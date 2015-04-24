062bb06abf2e65b27d7380cb6bda63a1e4a5729a
Bry Zettler <bryanzettler@gmail.com>
2015-04-24 17:16:30 -0500

    added title and message to blocker

--- source/blocker.js
+++ source/blocker.js
@@ -1,7 +1,23 @@
 enyo.kind({
   name: 'bootstrap.Blocker',
-  classes: 'onyx-scrim enyo-fit onyx-scrim-translucent spinner-container',
+  classes: 'onyx-scrim enyo-fit onyx-scrim-translucent spinner-wrapper',
+  published: {
+    title: null,
+    message: null
+  },
   components: [
-    { classes: 'fa fa-spinner fa-spin spinner' }
+    { classes: "spinner-container", components: [
+      {classes: "spinner-holder", components: [
+        { classes: 'fa fa-spinner fa-spin spinner' },
+        { name: "title", tag: "h2", classes: "spinner-title"},
+        { tag: "h2", classes: "spinner-message", components: [
+          { name: "message", tag: "small"}
+        ]}
+      ]}
+    ]}
+  ],
+  bindings: [
+    {from: ".title", to: ".$.title.content"},
+    {from: ".message", to: ".$.message.content"}
   ]
 });
