6bec647a7df645907d1aa5d5769f3a576bfc6ffb
Chris Hall <chall@pangeare.com>
2015-06-24 15:00:53 -0500

    Make ProgressBar kind work with dynamic values
    Moved all the setup logic into xChanged handlers.
    
    Changed the symantics of the `text` property slightly.  It's now used to
    determine if text should be displayed, not the text to display.  The
    text displayed is now always the percentage.

--- source/ProgressBars.js
+++ source/ProgressBars.js
@@ -1,42 +1,55 @@
 enyo.kind({
   name: "bootstrap.ProgressBar",
-	// ...........................
-	// PUBLIC PROPERTIES
+  // ...........................
+  // PUBLIC PROPERTIES
   classes: "progress",
   published: {
     percent: false,
     striped: false,
     text: false,
-    type: "default", // progress-bar-default || progress-bar-primary || progress-bar-success || progress-bar-info || progress-bar-warning || progress-bar-danger    
+    type: "default", // progress-bar-default || progress-bar-primary || progress-bar-success || progress-bar-info || progress-bar-warning || progress-bar-danger
   },
   components: [
     {tag: 'div', name: "progressBar", classes: "progress-bar", components: [
       {tag: 'span', name: "progressBarText", classes: "sr-only"}
     ]}
   ],
-	// ...........................
-	// PROTECTED METHODS
-	create: function() {
-		this.inherited(arguments);
-		this.setupBar();
-	},
-	setupBar: function(){
-		if(this.type && this.type != "default"){
-		  this.$.progressBar.addClass("progress-bar-" + this.type);
-		}
-    if(this.striped){
-		  this.$.progressBar.addClass("progress-bar-striped");
+  // ...........................
+  // PROTECTED METHODS
+  create: function() {
+    this.inherited(arguments);
+    this.setupBar();
+  },
+  setupBar: function(){
+    this.typeChanged();
+    this.stripedChanged();
+    this.percentChanged();
+    this.textChanged();
+  },
+  typeChanged: function(oldType) {
+    // remove old type class
+    if(oldType && oldType != "default"){
+      this.$.progressBar.removeClass("progress-bar-" + oldType);
     }
-		if(this.percent){
-		  this.$.progressBar.applyStyle("width", this.percent + '%');
-		}
-    if(this.text){
-      this.$.progressBarText.removeClass("sr-only");
-      if(this.percent){
-        this.$.progressBarText.setContent(this.text);        
-      } else {
-        this.$.progressBarText.setContent('0%');
-      }
+
+    // add the new
+    if(this.type && this.type != "default"){
+      this.$.progressBar.addClass("progress-bar-" + this.type);
     }
-	}
+  },
+  stripedChanged: function(){
+    this.$.progressBar.addRemoveClass("progress-bar-striped", this.striped);
+  },
+  percentChanged: function(oldPercent) {
+    if(this.percent){
+      this.$.progressBar.applyStyle("width", this.percent + "%");
+      this.$.progressBarText.setContent(this.percent + "%");
+    } else if(oldPercent) {
+      this.$.progressBar.applyStyle("width", null);
+      this.$.progressBarText.setContent("0%");
+    }
+  },
+  textChanged: function(oldText) {
+    this.$.progressBarText.addRemoveClass("sr-only", !this.text);
+  }
 });
